699bc48a12c8f790f22d220480ae4ea39ed5b022
CUI <garycui222@gmail.com>
2022-01-21 18:04:40 -0500

    create application successfuly

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,51 @@
 import React, { useState } from "react";
 
-
+import Appointment from "./Appointment";
 import "components/Application.scss";
 import DayList from "components/DayList";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
+
+
+
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
@@ -24,6 +67,19 @@ export default function Application(props) {
       spots: 0,
     },
   ];
+
+  const appoint = appointments.map((appointment) => {
+    return (
+      // <Appointment 
+      //   key={appointment.id} 
+      //   id={appointment.id}
+      //   time={appointment.time}
+      //   interview={appointment.interview}
+      // />
+      <Appointment key={appointment.id} {...appointment} />
+    );
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -34,13 +90,15 @@ export default function Application(props) {
         alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            value={day}
+            onChange={setDay}
+          />
+        </nav>
         
-        <DayList
-          days={days}
-          value={day}
-          onChange={setDay}
-        />
+        
 
         <img
           className="sidebar__lhl sidebar--centered"
@@ -51,6 +109,8 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appoint}
+        <Appointment key="last" time="5pm" />
       </section>
       
     </main>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,27 @@
 // import Appointment from "components/Appointment";
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 
 export default function Appointment(props) {
 
-    return ( 
-      props.time ?  <article className="appointment"> Appointment at {props.time}</article> : <article/>
-    );
-  }
+  return ( 
+    <article className='appointment'>
+        <Header time={props.time} />
+        {props.interview ? 
+        <>
+          <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/>
+        </>
+        :
+        <>
+          <Empty/>
+        </>
+        }
+    </article>
+        
+  );
+}
+
